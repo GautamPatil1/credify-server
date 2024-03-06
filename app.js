758227de1384dc7a13bbf73a81eab5fbf69180dd
GautamPatil <gautampatil4545@outlook.com>
2024-03-06 19:25:32 +0530

    Created Basic App

---
+++ app.js
@@ -0,0 +1,80 @@
+//libraries 
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ObjectId } = require('mongodb');
+
+//setup 
+const PORT = process.env.PORT || 3000;
+const app = express();
+const uri = "mongodb+srv://gautampatil:MGsxw0XPIc2qFGaK@gautam.5nbd8ho.mongodb.net/?retryWrites=true&w=majority";
+
+//middleware
+app.use(express.json());
+app.use(cors( ));
+
+//database connection
+const client = new MongoClient(uri);
+client.connect()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
+
+
+//routes
+app.get('/', (req, res) => {
+    res.json(g);
+})
+
+//get data from database
+app.get('/cert/:certId', async (req, res) => {
+    try {
+      const certId = req.params.certId;
+      const cert = await client
+        .db('credify')
+        .collection('credify-data')
+        .find({ certId })
+        .toArray();
+  
+      res.json(cert);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+//add data in database
+app.post('/cert', async (req, res) => {
+    try {
+      const cert = req.body;
+      const certId = cert.certId;
+      
+      // Ensure userId is included in the request body
+      if (!certId) {
+        return res.status(400).json({ error: 'certId is required in the request body' });
+      }
+
+      const result = await client
+        .db('credify')
+        .collection('credify-data')
+        .insertOne(cert);
+  
+      res.status(201).json({ ...cert, _id: result.insertedId }); // Return the inserted todo with its generated _id
+    } catch (error) {
+      console.error('Error adding Certificate:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.post('/', (req, res) => {
+    const data = req.body;
+    console.log(data);
+    res.json({"Response": data});
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
